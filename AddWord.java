5903efbd016af1e306d93b9805773dd8a548fc73
Tronikel <contactdonatas@gmail.com>
2024-05-12 11:35:04 +0300

    word adder

--- AddWord.java
+++ AddWord.java
@@ -3,20 +3,39 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.io.File;
+import java.io.FileWriter;
 
 public class AddWord {
+    private void appendToDb(String data) throws IOException {
+        File file = new File("words.txt");
+        FileWriter fr = new FileWriter(file, true);
+
+        fr.write("\n" + data);
+        fr.close();
+    }
+
     public void init() throws IOException {
         Pattern pattern = Pattern.compile("[a-zA-Z]");
         System.out.println("Write words to add, they can only contain english characters");
+        System.out.println("Press enter without typing anything to exit");
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
         String input = bufferedReader.readLine().trim();
+
+        if (input.length() == 0) {
+            return;
+        }
+
         Matcher matcher = pattern.matcher(input);
 
-        String matched = matcher.group(0).toLowerCase();
+        String matched = "";
 
-        if (matched.compareTo(input.toLowerCase()) != 0) {
+        while (matcher.find()) {
+            matched += matcher.group();
+        }
+
+        if (matched.compareToIgnoreCase(input) != 0) {
             System.out.println("invalid input");
 
             init();
@@ -25,5 +44,7 @@ public class AddWord {
 
         System.out.println("adding: " + matched);
 
+        appendToDb(matched);
+        init();
     }
 }
--- Hangman.java
+++ Hangman.java
@@ -3,7 +3,6 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
